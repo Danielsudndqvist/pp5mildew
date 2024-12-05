bf763c8cdba224b1d552e66e22557ed752221f03
Daniel Sundqvist <1891sunkan@gmail.com>
2024-12-05 18:20:42 +0000

    changed imports for main.py

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import streamlit as st
-from app_pages import prediction_page, metrics_page
+from app_pages.prediction import app as prediction_app
+from app_pages.metrics import page_metrics
 
-st.set_page_config(page_title="Mildew Detection", layout="wide")
+st.set_page_config(page_title="Cherry Leaf Mildew Detection", layout="wide")
 
+# Create dictionary of pages
 pages = {
-    "Prediction": prediction_page,
-    "Model Metrics": metrics_page,
+    "Home": lambda: st.write("Welcome to Cherry Leaf Mildew Detection App"),
+    "Make Prediction": prediction_app,
+    "Model Performance": page_metrics,
 }
 
 st.sidebar.title("Navigation")
 selection = st.sidebar.radio("Go to", list(pages.keys()))
 
-pages[selection].app()
+pages[selection]()
