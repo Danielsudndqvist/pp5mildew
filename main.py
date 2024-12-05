608602d67e8478d4061022895fbc69809aecde25
Daniel Sundqvist <1891sunkan@gmail.com>
2024-12-05 18:21:42 +0000

    changes in main

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import streamlit as st
+from app_pages.home import app as home_app
 from app_pages.prediction import app as prediction_app
 from app_pages.metrics import page_metrics
 
 st.set_page_config(page_title="Cherry Leaf Mildew Detection", layout="wide")
 
-# Create dictionary of pages
 pages = {
-    "Home": lambda: st.write("Welcome to Cherry Leaf Mildew Detection App"),
+    "Home": home_app,
     "Make Prediction": prediction_app,
     "Model Performance": page_metrics,
 }
