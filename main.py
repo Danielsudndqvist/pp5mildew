a857e69c52349f7ff4d131a17e843bee767849db
Daniel Sundqvist <1891sunkan@gmail.com>
2024-12-09 18:09:17 +0000

    uppdated main page to include all pages

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
 import streamlit as st
 from app_pages.home import app as home_app
 from app_pages.prediction import app as prediction_app
-from app_pages.metrics import page_metrics
+from app_pages.visualization import app as visualization_app
+from app_pages.metrics import app as metrics_app
 
-st.set_page_config(page_title="Cherry Leaf Mildew Detection", layout="wide")
+def main():
+    st.set_page_config(page_title="Cherry Leaf Disease Detector", layout="wide")
+    
+    pages = {
+        "Home": home_app,
+        "Leaf Analysis": visualization_app,
+        "Make Prediction": prediction_app,
+        "Model Performance": metrics_app
+    }
+    
+    with st.sidebar:
+        st.image("assets/leaves_logo.png", width=200)
+        st.title("Navigation")
+        selection = st.radio("Go to", list(pages.keys()))
+        
+        st.info("""
+        This application helps detect powdery mildew infection in cherry leaves using machine learning.
+        """)
+    
+    pages[selection]()
 
-pages = {
-    "Home": home_app,
-    "Make Prediction": prediction_app,
-    "Model Performance": page_metrics,
-}
-
-st.sidebar.title("Navigation")
-selection = st.sidebar.radio("Go to", list(pages.keys()))
-
-pages[selection]()
+if __name__ == "__main__":
+    main()
