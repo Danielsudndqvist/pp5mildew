387f2d260828134e1d630e605e4b9b1752fd19b4
Daniel Sundqvist <1891sunkan@gmail.com>
2024-12-11 16:58:38 +0000

    changed imports for deployment

--- tests/test_app_pages.py
+++ tests/test_app_pages.py
@@ -1,8 +1,11 @@
-# tests/test_app_pages.py
 import streamlit as st
 from app_pages.home import app as home_app
 from app_pages.prediction import app as prediction_app
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+from src.model.prediction import process_image, predict_mildew
 def test_home_page():
     home_app()
     assert st.title == "Cherry Leaf Mildew Detection"
--- tests/test_prediction.py
+++ tests/test_prediction.py
@@ -2,6 +2,11 @@ import pytest
 import numpy as np
 from PIL import Image
 from src.model.prediction import process_image
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from src.model.prediction import process_image
 
 def test_process_image():
     """Test basic image processing"""
